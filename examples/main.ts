0dab3e83ac46a1edd65e2ee6c47d67b03d039853
Luis Blanco <luisblanco1337@gmail.com>
2025-03-14 17:57:33 +0400

    Update example

--- examples/main.ts
+++ examples/main.ts
@@ -1,61 +1,38 @@
 import * as bullet from 'bullet-raub';
 const { Scene, Body, Joint } = bullet;
-console.log('bb', Scene, Body, Joint);
+
 
 const scene = new Scene();
-console.log('SCENE', scene, scene.gravity);
 scene.on('gravity', (g) => console.log('gravity event', g));
 scene.gravity = [0, -9.8, 0];
 
 const plane = new Body({ scene });
 plane.type = 'plane';
-console.log('PLANE', plane);
-
 
 const bodyA = new Body({ scene });
 bodyA.pos = [0, 100, 0];
 bodyA.mass = 1;
 
-let prevTimeA = 0;
-bodyA.on('update', (u) => {
-	const t = Date.now();
-	if (prevTimeA + 1000 > t) {
-		return;
-	}
-	
-	prevTimeA = t;
-	console.log('update A', u.pos.y);
-});
-
 const bodyB = new Body({ scene });
 bodyB.pos = [0, 101, 0];
 bodyB.mass = 1;
 
-console.log('A', bodyA);
-console.log('B', bodyB);
-
-
 const joint = new Joint();
-console.log('JOINT', joint);
-joint.on('a', (a) => console.log('joint A', a));
-joint.on('b', (a) => console.log('joint B', a));
-
-let prevTimeJ = 0;
-joint.on('update', (u) => {
-	const t = Date.now();
-	if (prevTimeJ + 1000 > t) {
-		return;
-	}
-	
-	prevTimeJ = t;
-	console.log('update J', u.posa.y, u.posb.y);
-});
+joint.minl = [0, 1, 0];
+joint.maxl = [0, 1, 0];
+// joint.on('a', (a) => console.log('joint A', a));
+// joint.on('b', (a) => console.log('joint B', a));
 
 joint.a = bodyA;
-console.log('J-A', joint.a);
-
 joint.b = bodyB;
-console.log('J-B', joint.b);
+
+
+console.log('scene =', scene);
+console.log('plane =', plane);
+console.log('A =', bodyA);
+console.log('B =', bodyB);
+console.log('JOINT =', joint);
+
 
 // TODO
 // const trace1 = new Trace({ scene, from: [0, 200, 0], to: [0, -10, 0] });
@@ -66,10 +43,33 @@ console.log('J-B', joint.b);
 
 
 const hitRes = scene.hit([0, 200, 0], [0, -10, 0]);
-console.log('hitRes', hitRes);
+console.log('Body hit by ray:', hitRes);
 
 const traceRes = scene.trace([0, 200, 0], [0, -10, 0]);
-console.log('traceRes', traceRes);
+console.log('Ray traced bodies:', traceRes.map((result) => result.body));
+
+
+let prevTimeA = 0;
+bodyA.on('update', (u) => {
+	const t = Date.now();
+	if (prevTimeA + 1000 > t) {
+		return;
+	}
+	
+	prevTimeA = t;
+	console.log('Body A height:', u.pos.y);
+});
+
+let prevTimeJ = 0;
+joint.on('update', (u) => {
+	const t = Date.now();
+	if (prevTimeJ + 1000 > t) {
+		return;
+	}
+	
+	prevTimeJ = t;
+	console.log('Joint body heights:', u.posa.y, u.posb.y);
+});
 
 
 const frame = () => {
